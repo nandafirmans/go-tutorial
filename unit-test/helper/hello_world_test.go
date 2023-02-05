a7b6a3acc216c8ada1563d1d652a222bc0b8ea9a
Nanda Firmansyah <nandafirmans@gmail.com>
2023-02-05 11:17:46 +0700

    update unit test

--- unit-test/helper/hello_world_test.go
+++ unit-test/helper/hello_world_test.go
@@ -1,11 +1,26 @@
 package helper
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
-func TestHelloWorld(t *testing.T) {
+func TestHelloWorldNanda(t *testing.T) {
 	result := HelloWorld("Nanda")
 
 	if result != "Hello Nanda" {
-		panic("Result is not Hello Nanda")
+		t.Fail()
 	}
+
+	fmt.Println("TestHelloWorldNanda Done")
+}
+
+func TestHelloWorldAfif(t *testing.T) {
+	result := HelloWorld("Afif 1")
+
+	if result != "Hello Afif" {
+		t.FailNow()
+	}
+
+	fmt.Println("TestHelloWorldAfif Done")
 }
