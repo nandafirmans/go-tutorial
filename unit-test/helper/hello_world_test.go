aebdfade0e35407c784bacc1534fad193584d2d9
Nanda Firmansyah <nandafirmans@gmail.com>
2023-02-05 14:49:01 +0700

    install tetify

--- unit-test/helper/hello_world_test.go
+++ unit-test/helper/hello_world_test.go
@@ -3,7 +3,27 @@ package helper
 import (
 	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
+
+func TestHelloWorldRequire(t *testing.T) {
+	// assert menggunakan t.FailNow sama seperti t.Fatal
+	// jadi apabila test fail maka esekusi akan berhenti
+	result := HelloWorld("Nanda")
+	require.Equal(t, result, "Hello Nanda", "Result must be 'Hello Nanda'")
+	//code dibawah ini tidak akan esekusi apabila test fail
+	fmt.Println("TestHelloWorldRequire Done")
+}
+
+func TestHelloWorldAssert(t *testing.T) {
+	// assert menggunakan t.Fail sama seperti t.Error
+	// jadi apabila test fail fungsi setelah nya akan tetap diesekusi
+	result := HelloWorld("Nanda")
+	assert.Equal(t, result, "Hello Nanda", "Result must be 'Hello Nanda'")
+	fmt.Println("TestHelloWorldAssert Done")
+}
 
 func TestHelloWorldNanda(t *testing.T) {
 	result := HelloWorld("Nanda")
