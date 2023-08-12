8b45c87b9bae57e94b1097adc19fa704bee10d49
Nanda Firmansyah <nandafirmans@gmail.com>
2023-08-12 14:54:54 +0700

    update

--- database/database_test.go
+++ database/database_test.go
@@ -1,6 +1,24 @@
 package database
 
 import (
-	_ "github.com/go-sql-driver/mysql"
+	"database/sql"
 	"testing"
+
+	_ "github.com/go-sql-driver/mysql"
 )
+
+func TestEmpty(t *testing.T) {
+
+}
+
+func TestOpenConnection(t *testing.T) {
+	db, err := sql.Open("mysql", "root:1722@tcp(localhost:3306)/go_test")
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer db.Close()
+}
+
+// NOTE: golang secara default sudah memiliki kemampuan database pooling. database pooling adalah manajemen koneksi database sehingga kita tidak perlu melakukannya secara manual.
