4f02a5ab0ed87ad6e35c31287dd8c47055490f6c
nandafirmans <nandafirmans@gmail.com>
2023-08-12 16:29:54 +0700

    go sql: TestQueryContext

--- database/sql_test.go
+++ database/sql_test.go
@@ -24,3 +24,37 @@ func TestExecContext(t *testing.T) {
 
 	fmt.Println("Success insert data")
 }
+
+func TestQueryContext(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+
+	script := "SELECT id, name FROM customer"
+
+	// NOTE: QueryContext digunakan untuk eksekusi query yang mengembalikan data, seperti SELECT.
+	// NOTE: rows disini bentuknya adalah pointer jadi hanya bisa Next() terus untuk melakukan iterasi data hasil query.
+	rows, err := db.QueryContext(ctx, script)
+
+	if err != nil {
+		panic(err)
+	}
+
+	// NOTE: defer rows.Close() digunakan untuk menutup koneksi database setelah selesai melakukan query.
+	defer rows.Close()
+
+	for rows.Next() {
+		var id, name string
+
+		err := rows.Scan(&id, &name)
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println("ID:", id)
+		fmt.Println("Name:", name)
+	}
+
+	fmt.Println("Success get data")
+}
