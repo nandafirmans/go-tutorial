457b0fa5a2cd060c2b5c58c087f4318ef4f6543f
nandafirmans <nandafirmans@gmail.com>
2023-08-12 16:13:44 +0700

    add notes

--- database/sql_test.go
+++ database/sql_test.go
@@ -10,10 +10,14 @@ func TestExecContext(t *testing.T) {
 	db := GetConnection()
 	defer db.Close()
 
+	// NOTE: menggunakan context agar bisa melakukan cancel saat ExecContext sedang berjalan.
 	ctx := context.Background()
 
-	script := "INSERT INTO customer(id, name) VALUES('NANDA', 'NANDA')"
+	script := "INSERT INTO customer(id, name) VALUES('BUDI', 'BUDI')"
+
+	// NOTE: ExecContext digunakan untuk eksekusi query yang tidak mengembalikan data, seperti INSERT, UPDATE, DELETE, dan lain-lain.
 	_, err := db.ExecContext(ctx, script)
+
 	if err != nil {
 		panic(err)
 	}
