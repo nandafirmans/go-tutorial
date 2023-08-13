602b2df86c2dac12a4ada313a8788e96a0e70e98
nandafirmans <nandafirmans@gmail.com>
2023-08-13 12:43:36 +0700

    update sql script

--- database/script.sql
+++ database/script.sql
@@ -1,21 +1,21 @@
 create table customer
 (
-	id   varchar(100) not null,
-	name varchar(100) not null,
-	primary key(id)
-) engine = InnoDb;
+  id   varchar(100) not null,
+  name varchar(100) not null,
+  primary key(id)
+) engine = InnoDB;
 
 delete from customer 
 
 select * from customer
 
 alter table customer 
-	add column email varchar(100),
-	add column balance integer default 0,
-	add column rating double  default 0.0,
-	add column created_at timestamp default CURRENT_TIMESTAMP,
-	add column birth_date DATE,
-	add column married boolean default false;
+  add column email varchar(100),
+  add column balance integer default 0,
+  add column rating double  default 0.0,
+  add column created_at timestamp default CURRENT_TIMESTAMP,
+  add column birth_date DATE,
+  add column married boolean default false;
 
 DESC customer 
 
@@ -26,4 +26,14 @@ VALUES
 	('joko', 'Joko', 'Joek@email.com', 53000000, 53.0, '2012-10-10', false),
 	('joni', 'Joni', 'jonnnn@email.com', 23000000, 78.0, '1995-10-10', false);
 
+UPDATE customer 
+SET email = NULL,
+  birth_date = NULL
+WHERE id = 'joko'
 
+CREATE TABLE user
+(
+  username VARCHAR(100) NOT NULL,
+  password VARCHAR(100) NOT NULL,
+  PRIMARY KEY(username)
+) ENGINE = InnoDB;
