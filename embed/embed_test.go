6a27f0baad549c6b9a94f96b76786f2ed7b9deb4
nandafirmans <nandafirmans@gmail.com>
2023-08-15 13:07:02 +0700

    go embed

---
+++ embed/embed_test.go
@@ -0,0 +1,43 @@
+package embed
+
+import (
+	"embed"
+	_ "embed"
+	"fmt"
+	"io/fs"
+	"io/ioutil"
+	"testing"
+)
+
+//go:embed version.txt
+var vesion string
+
+func TestEmbedString(t *testing.T) {
+	fmt.Println("Version", vesion)
+}
+
+//go:embed cat.png
+var catImage []byte
+
+func TestEmbedByte(t *testing.T) {
+	err := ioutil.WriteFile("cat_copy.png", catImage, fs.ModePerm)
+	if err != nil {
+		panic(err)
+	}
+}
+
+//go:embed files/a.txt
+//go:embed files/b.txt
+//go:embed files/c.txt
+var files embed.FS
+
+func TestEmbedFS(t *testing.T) {
+	a, _ := files.ReadFile("files/a.txt")
+	fmt.Println(string(a))
+
+	b, _ := files.ReadFile("files/b.txt")
+	fmt.Println(string(b))
+
+	c, _ := files.ReadFile("files/c.txt")
+	fmt.Println(string(c))
+}
