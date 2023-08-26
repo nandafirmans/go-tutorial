05e54b66c56914607475abfd5f18d80165a0fd52
nandafirmans <nandafirmans@gmail.com>
2023-08-26 16:12:19 +0700

    go web: handler

--- web/handler_test.go
+++ web/handler_test.go
@@ -7,9 +7,41 @@ import (
 )
 
 func TestHandler(t *testing.T) {
+	// NOTE: HandlerFunc adalah simple implementation dari interface Handler yang hanya memiliki satu method ServeHTTP
+
 	var handler http.HandlerFunc = func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "Hello World")
 	}
+
+	server := http.Server{
+		Addr:    "localhost:3001",
+		Handler: handler,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func TestHandlerServeMux(t *testing.T) {
+	handler := http.NewServeMux()
+
+	handler.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hello World")
+	})
+
+	handler.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "<h1>Hi</h1>")
+	})
+
+	handler.HandleFunc("/images/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "<h1>Images</h1>")
+	})
+
+	handler.HandleFunc("/images/thumbnails/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "<h1>Images Thumbnails</h1>")
+	})
 
 	server := http.Server{
 		Addr:    "localhost:3001",
