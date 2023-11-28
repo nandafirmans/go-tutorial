975d93a314d381f90d28bf0cf34b243fc568d839
Nanda Firmansyah <nandafirmans@gmail.com>
2023-11-28 13:07:16 +0700

    go web: action template

--- web/12_html_template_action_test.go
+++ web/12_html_template_action_test.go
@@ -26,3 +26,39 @@ func TestTemplateActionIf(t *testing.T) {
 	body, _ := io.ReadAll(recorder.Result().Body)
 	fmt.Println(string(body))
 }
+
+func TemplateActionIfCompare(w http.ResponseWriter, r *http.Request) {
+	t := template.Must(template.ParseFiles("./12_templates/if-compare.html"))
+	t.ExecuteTemplate(w, "if-compare.html", map[string]interface{}{
+		"Title": "Lorem Ipsum",
+		"Value": 80,
+	})
+}
+
+func TestTemplateActionIfCompare(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8087", nil)
+	recorder := httptest.NewRecorder()
+
+	TemplateActionIfCompare(recorder, request)
+
+	body, _ := io.ReadAll(recorder.Result().Body)
+	fmt.Println(string(body))
+}
+
+func TemplateActionRange(w http.ResponseWriter, r *http.Request) {
+	t := template.Must(template.ParseFiles("./12_templates/range.html"))
+	t.ExecuteTemplate(w, "range.html", map[string]interface{}{
+		"Title": "Lorem Ipsum",
+		"Names": []string{"Agus", "Budi", "Cecep", "Doni", "Eko"},
+	})
+}
+
+func TestTemplateActionRange(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8087", nil)
+	recorder := httptest.NewRecorder()
+
+	TemplateActionRange(recorder, request)
+
+	body, _ := io.ReadAll(recorder.Result().Body)
+	fmt.Println(string(body))
+}
